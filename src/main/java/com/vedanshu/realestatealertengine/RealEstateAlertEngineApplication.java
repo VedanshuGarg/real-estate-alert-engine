00c973c6cc74ebf244729110a4ec29dfccbca894
Vedanshu Garg <garg.vedanshu04@gmail.com>
2026-03-22 16:06:15 +0530

    [REAL-001] chore: Initialize real estate alert engine architecture

---
+++ src/main/java/com/vedanshu/realestatealertengine/RealEstateAlertEngineApplication.java
@@ -0,0 +1,13 @@
+package com.vedanshu.realestatealertengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealEstateAlertEngineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealEstateAlertEngineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vedanshu/realestatealertengine/RealEstateAlertEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vedanshu.realestatealertengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealEstateAlertEngineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
